91b0951f52be3b820049300e5f9acaf58703d215
Vlad Burtsev <burthev04@gmail.com>
2023-12-22 18:11:25 +0200

    Modify controller, added: delete, chnge, get one, get all, add rating to device

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,5 +1,12 @@
 import { ApiError } from "../error/ApiError.js";
-import { Device, DeviceInfo } from "../models/models.js";
+import {
+  Device,
+  DeviceInfo,
+  DeviceRating,
+  Type,
+  Brand,
+  User,
+} from "../models/models.js";
 import { v4 as uuidv4 } from "uuid";
 
 import sharp from "sharp";
@@ -65,6 +72,178 @@ export const createDevice = async (req, res, next) => {
     }
 
     return res.status(200).json({ createdDevice });
+  } catch (error) {
+    return next(ApiError.badRequest(error.message));
+  }
+};
+
+export const getOneDevice = async (req, res, next) => {
+  try {
+    const { slug } = req.params;
+
+    const existingDevice = await Device.findOne({
+      where: { slug },
+      include: [
+        { model: DeviceRating, as: "rating" },
+        { model: DeviceInfo, as: "info" },
+        { model: Brand, as: "brand", attributes: ["name"] },
+        { model: Type, as: "type", attributes: ["name"] },
+      ],
+    });
+
+    if (!existingDevice) {
+      return next(ApiError.notFound("Device not found"));
+    }
+
+    return res.status(200).json(existingDevice);
+  } catch (error) {
+    return next(ApiError.badRequest(error.message));
+  }
+};
+
+export const deleteDevice = async (req, res, next) => {
+  try {
+    const { slug } = req.params;
+    const existingDevice = await Device.findOne({
+      where: { slug },
+    });
+    if (!existingDevice) {
+      return next(ApiError.notFound("Device not found"));
+    }
+
+    await existingDevice.destroy();
+
+    return res.status(200).json("Device successfully deleted.");
+  } catch (error) {
+    return next(ApiError.badRequest(error.message));
+  }
+};
+
+export const changeDevice = async (req, res, next) => {
+  try {
+    const { slug } = req.params;
+    const { name, price, description } = req.body;
+
+    const existingDevice = await Device.findOne({
+      where: { slug },
+    });
+
+    if (!existingDevice) {
+      return next(ApiError.notFound("Device not found"));
+    }
+
+    existingDevice.name = name || existingDevice.name;
+    existingDevice.price = price || existingDevice.price;
+    existingDevice.description = description || existingDevice.description;
+    if (name) {
+      const newSlug = convertSlug(name);
+      existingDevice.slug = newSlug || existingDevice.slug;
+    }
+
+    await existingDevice.save();
+
+    return res.status(200).json(existingDevice);
+  } catch (error) {
+    return next(ApiError.badRequest(error.message));
+  }
+};
+
+export const getAllDevices = async (req, res, next) => {
+  try {
+    const { limit = 12, page = 1, typeId, brandId } = req.query;
+
+    let offset = page * limit - limit;
+
+    let devices;
+
+    if (!typeId && !brandId) {
+      devices = await Device.findAndCountAll({
+        limit,
+        offset,
+        include: [
+          { model: DeviceRating, as: "rating" },
+          { model: DeviceInfo, as: "info" },
+          { model: Brand, as: "brand", attributes: ["name"] },
+          { model: Type, as: "type", attributes: ["name"] },
+        ],
+      });
+    }
+
+    if (typeId && !brandId) {
+      devices = await Device.findAndCountAll({
+        where: { typeId },
+        limit,
+        offset,
+        include: [
+          { model: DeviceRating, as: "rating" },
+          { model: DeviceInfo, as: "info" },
+          { model: Brand, as: "brand", attributes: ["name"] },
+          { model: Type, as: "type", attributes: ["name"] },
+        ],
+      });
+    }
+
+    if (!typeId && brandId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId },
+        limit,
+        offset,
+        include: [
+          { model: DeviceRating, as: "rating" },
+          { model: DeviceInfo, as: "info" },
+          { model: Brand, as: "brand", attributes: ["name"] },
+          { model: Type, as: "type", attributes: ["name"] },
+        ],
+      });
+    }
+
+    if (typeId && brandId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId, brandId },
+        limit,
+        offset,
+        include: [
+          { model: DeviceRating, as: "rating" },
+          { model: DeviceInfo, as: "info" },
+          { model: Brand, as: "brand", attributes: ["name"] },
+          { model: Type, as: "type", attributes: ["name"] },
+        ],
+      });
+    }
+
+    return res.status(200).json(devices);
+  } catch (error) {
+    return next(ApiError.badRequest(error.message));
+  }
+};
+
+export const addRating = async (req, res, next) => {
+  try {
+    const { rating } = req.body;
+    const { slug } = req.params;
+    const userId = req.user.id;
+    const existingUser = await User.findOne({ where: { id: userId } });
+    if (!existingUser) {
+      return next(ApiError.badRequest("User does not exist"));
+    }
+
+    const existingDevice = await Device.findOne({ where: { slug: slug } });
+
+    if (!existingDevice) {
+      return next(ApiError.badRequest("Device does not exist"));
+    }
+
+    const [existingRating, created] = await DeviceRating.findOrCreate({
+      where: { userId, slug },
+      defaults: { rating },
+    });
+
+    if (!created) {
+      await existingRating.update({ rating });
+      return res.status(200).json(existingRating);
+    } else {
+      return res.status(201).json(existingRating);
+    }
   } catch (error) {
     return next(ApiError.badRequest(error.message));
   }
--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -4,15 +4,34 @@ import { Router } from "express";
 import { checkRoleMiddleware } from "../middleware/checkRoleMiddleware.js";
 
 //controllers
-import { createDevice } from "../controllers/deviceController.js";
+import {
+  addRating,
+  changeDevice,
+  createDevice,
+  deleteDevice,
+  getAllDevices,
+  getOneDevice,
+} from "../controllers/deviceController.js";
+import { checkAuthMiddleware } from "../middleware/checkAuthMiddleware.js";
 
 export const deviceRouter = new Router();
 
 //user routes
-deviceRouter.get("/get_all_devices");
-deviceRouter.get("/get_one_device/:slug");
+deviceRouter.get("/get_all_devices", getAllDevices);
+deviceRouter.get("/get_one_device/:slug", getOneDevice);
+
+//auth users
+deviceRouter.post("/add_rating/:slug", checkAuthMiddleware, addRating);
 
 //admin routes
 deviceRouter.post("/create_device", checkRoleMiddleware("ADMIN"), createDevice);
-deviceRouter.put("/change_device/:slug", checkRoleMiddleware("ADMIN"));
-deviceRouter.delete("/delete_device/:slug", checkRoleMiddleware("ADMIN"));
+deviceRouter.put(
+  "/change_device/:slug",
+  checkRoleMiddleware("ADMIN"),
+  changeDevice
+);
+deviceRouter.delete(
+  "/delete_device/:slug",
+  checkRoleMiddleware("ADMIN"),
+  deleteDevice
+);
